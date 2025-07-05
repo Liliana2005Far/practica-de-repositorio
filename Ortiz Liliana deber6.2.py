0052f8169bc36e2e3940da34f4bd8891e9bd0978
Liliana2005Far <liliana.ortiz@epn.edu.ec>
2025-07-05 10:26:29 -0500

    cambio 148

--- Ortiz Liliana deber6.2.py
+++ Ortiz Liliana deber6.2.py
@@ -145,7 +145,7 @@ def main():
             print("Gracias por usar el sistema.")
             break
         else:
-            print("Opción inválida. Intente nuevamente.")
+            print("RROR .")
 
 main()
  
