5f51311ead740c16699429ed3ef6d4ba79d86096
Liliana2005Far <liliana.ortiz@epn.edu.ec>
2025-07-14 22:22:59 -0500

    Primer borrador

--- eje.cpp
+++ eje.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    std:: cout<<"Hola"<<std::endl;
+int main() {
+    string passwordCorrecto = "clave123";
+    string intento;
+    int intentos = 0;
+    const int MAX_INTENTOS = 3;
+
+    while (intentos < MAX_INTENTOS) {
+        cout << "Ingrese el password: ";
+        cin >> intento;
+
+        if (intento == passwordCorrecto) {
+            cout << "Acceso concedido!" << endl;
+            break;
+        } else {
+            cout << "ERROR Password incorrecto." << endl;
+            intentos++;
+        }
+    }
+
+    if (intentos == MAX_INTENTOS) {
+        cout << "Acceso denegado. Se agotaron los intentos." << endl;
+    }
+
     return 0;
 }
